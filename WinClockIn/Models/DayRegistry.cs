02b496f21ef4dea41a8aa4e6fab4a0181962a877
João Barreto <barretobit@gmail.com>
2025-06-30 22:22:23 +0200

    Adding the DayRegistry class;

---
+++ WinClockIn/Models/DayRegistry.cs
@@ -0,0 +1,13 @@
+﻿namespace WinClockIn.Models
+{
+    public class DayRegistry
+    {
+        public DateTime LogIn { get; set; }
+        public DateTime LunchBreakIn { get; set; }
+        public DateTime LunchBreakOff { get; set; }
+        public DateTime LogOff { get; set; }
+        public int TotalTime { get; set; }
+        public int LunchTime { get; set; }
+        public int SmallBreaksTime { get; set; }
+    }
+}
